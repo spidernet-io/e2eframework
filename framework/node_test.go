33c060ecacc16f0b5629b1651b13eab424c1149c
yulng <wei.yang@daocloud.io>
2022-06-24 19:00:42 +0800

    add timeout for WaitClusterNodeReady api

--- framework/node_test.go
+++ framework/node_test.go
@@ -5,6 +5,7 @@ package framework_test
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -72,30 +73,48 @@ func createNode(f *e2e.Framework, node *corev1.Node, opts ...client.CreateOption
 
 var _ = Describe("test node list ", Label("node"), func() {
 	var f *e2e.Framework
-	var nodeName string
-	var label map[string]string
-
+	var nodeName, nodeName2 string
+	var label, label2 map[string]string
+	var wg sync.WaitGroup
 	BeforeEach(func() {
 		f = fakeFramework()
 		nodeName = "spider-worker"
+		nodeName2 = "spider-worker2"
 		label = map[string]string{
 			"kubernetes.io/hostname": "spider-worker",
+		}
+		label2 = map[string]string{
+			"kubernetes.io/hostname": "spider-worker2",
 		}
 	})
 
 	It("operate nodelist", func() {
 
-		// create node yaml
-		node1 := generateExampleNodeYaml(nodeName, label)
-		Expect(node1).NotTo(BeNil())
-		GinkgoWriter.Printf("finish creating node %v \n", nodeName)
-		GinkgoWriter.Printf("node1: %v\n", node1)
+		wg.Add(1)
+		go func() {
+			GinkgoRecover()
+			// create node yaml
+			node1 := generateExampleNodeYaml(nodeName, label)
+			Expect(node1).NotTo(BeNil())
+			GinkgoWriter.Printf("finish creating node %v \n", nodeName)
+			GinkgoWriter.Printf("node1: %v\n", node1)
 
-		// create node
-		e1 := createNode(f, node1)
-		Expect(e1).NotTo(HaveOccurred())
+			// create node
+			e1 := createNode(f, node1)
+			Expect(e1).NotTo(HaveOccurred())
+			wg.Done()
+		}()
 
-		// getpodlist
+		// wait cluster node ready
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+		defer cancel()
+		bll, errcheck := f.WaitClusterNodeReady(ctx)
+		Expect(bll).NotTo(BeFalse())
+		Expect(errcheck).NotTo(HaveOccurred())
+
+		wg.Wait()
+
+		// get node list
 		opts := []client.ListOption{
 			client.MatchingLabels(map[string]string{
 				"kubernetes.io/hostname": "spider-worker",
@@ -106,8 +125,44 @@ var _ = Describe("test node list ", Label("node"), func() {
 		Expect(e1).NotTo(HaveOccurred())
 		GinkgoWriter.Printf("nodeList: %v", nodeList)
 		GinkgoWriter.Printf("corev1.NodeReady: %v", corev1.NodeReady)
+
+		// check node status
 		bl := f.CheckNodeStatus(&nodeList.Items[0], true)
 		Expect(bl).NotTo(BeFalse())
+
+		// status is set to false in order to use node not ready
+		wg.Add(1)
+		go func() {
+			GinkgoRecover()
+			// create node yaml
+			node2 := generateExampleNodeYaml(nodeName2, label2)
+			Expect(node2).NotTo(BeNil())
+			node2.Status.Conditions = []corev1.NodeCondition{
+				{
+					Status: corev1.ConditionFalse,
+				},
+			}
+			GinkgoWriter.Printf("finish creating node %v \n", nodeName2)
+			GinkgoWriter.Printf("node2: %v\n", node2)
+
+			// create node
+			e1 := createNode(f, node2)
+			Expect(e1).NotTo(HaveOccurred())
+			wg.Done()
+		}()
+
+		// wait cluster node ready
+		ctx2, cancel2 := context.WithTimeout(context.Background(), time.Second*20)
+		defer cancel2()
+		bll2, errcheck2 := f.WaitClusterNodeReady(ctx2)
+		Expect(bll2).NotTo(BeFalse())
+		Expect(errcheck2).NotTo(HaveOccurred())
+		wg.Wait()
+
+		// check node status
+		bl2 := f.CheckNodeStatus(&nodeList.Items[0], true)
+		Expect(bl2).To(BeTrue())
+
 	})
 
 	It("counter example with wrong input", func() {
@@ -121,7 +176,13 @@ var _ = Describe("test node list ", Label("node"), func() {
 		Expect(bl).NotTo(BeTrue())
 
 		// unit test IsClusterNodeReady
-		bll, errcheck := f.IsClusterNodeReady()
+		bll := f.CheckNodeStatus(node3, false)
+		Expect(bll).NotTo(BeTrue())
+
+		// unit test WaitClusterNodeReady
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		bll, errcheck := f.WaitClusterNodeReady(ctx)
 		Expect(bll).NotTo(BeFalse())
 		Expect(errcheck).NotTo(HaveOccurred())
 	})
